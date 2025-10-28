cde551e91ebaa36be54dd25421372f42a322c0d5
awesomearjun <iamawesomearjun@gmail.com>
2025-10-28 17:56:01 -0400

    Initial Commit

---
+++ learn-metal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  learn-metal
+//
+//  Created by OnionLord on 2025-10-28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return EXIT_SUCCESS;
+}
